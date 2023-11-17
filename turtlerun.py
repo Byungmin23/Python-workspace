baf3b050b4335759ef15d99132ff05132c7e6c41
ByungMin Yun <150504488+Byungmin23@users.noreply.github.com>
2023-11-17 19:08:28 +0900

    Update turtlerun.py

--- turtlerun.py
+++ turtlerun.py
@@ -1,61 +1,107 @@
 import turtle as t
 import random
 
-te = t.Turtle() #악당 거북이
+te = t.Turtle() # 악당 거북이(빨간색))
 te.shape("turtle")
 te.color("red")
 te.speed(0)
 te.up()
 te.goto(0, 200)
 
-ts = t.Turtle() #먹이
-ts.shape("circle")
-ts.color("green")
-ts.speed(0)
-ts.up()
-ts.goto(0, -200)
+point = t.Turtle() # 먹이(초록색 동그라미)
+point.shape("circle")
+point.color("green")
+point.speed(0)
+point.up()
+point.goto(0, -200)
 
-def turn_right(): #오른쪽으로 전환
+game_over_message = t.Turtle()
+score_message = t.Turtle()
+
+
+def game_over_message(m1, m2):
+    t.clear()
+    t.goto(0, 100)
+    t.write(m1, False, "center", ("", 20))
+    t.goto(0, -100)
+    t.write(m2, False, "center", ("", 20))
+    t.home()
+
+def score_message(m1,m2):
+    t.clear()
+    t.goto(-30, 100)
+    t.write(m1, False, "center", ("", 20, "bold"))
+    t.goto(30, 100)
+    t.write(m2, False, "center", ("", 20, "bold"))
+    
+
+def turn_right(): # 오른쪽으로 방향 전환
     t.setheading(0)
-
-def turn_up(): #위쪽으로 전환
+def turn_up(): # 위쪽으로 방향 전환
     t.setheading(90)
-
-def turn_left(): #왼쪽으로 전환
+def turn_left(): # 왼쪽으로 방향 전환
     t.setheading(180)
-
-def turn_down(): #아래쪽으로 전환
+def turn_down(): # 아래쪽으로 방향 전환
     t.setheading(270)
 
-def play(): #게임 진행 함수
-    t.forward(10) #주인공 거북이가 10만큼 앞으로 이동
+playing = False # 현재 게임이 플레이 중인지 확인하는 변수
 
-    ang = te.towards(t.pos()) #악당 거북이가 주인공 거북이를 향해 전환
-    te.setheading(ang)
+def start(): # 게임을 시작하는 함수
+    global playing
+    
+    if playing == False:
+        playing = True
+        t.clear() # 화면에 메시지를 지운다
+        play()
 
-    te.forward(9) #악당 거북이가 9만큼 이동
+score = 0 # 점수를 저장하는 변수
 
-    if t.distance(ts) < 12: #주인공과 먹이의 거리가 12보다 작으면
+def play():
+    global score
+    global playing
+
+    t.forward(10)
+
+    if random.randint(1, 5) == 3:
+        ang = te.towards(t.pos())
+        te.setheading(ang)
+
+    speed = score + 5
+
+    if speed > 15:
+        speed = 15
+
+    te.forward(speed)
+    if t.distance(te) < 12:
+        text = "Score : " + str(score)
+        game_over_message("Game Over", text)
+        playing = False
+        score = 0
+
+    if t.distance(point) < 12: # 먹이의 거리가 12보다 작으면
+        score = score + 1 # 점수를 올리고
+        score_message("Score : ", score) # 화면에 점수를 출력 합니다.
         start_x = random.randint(-230, 230)
         start_y = random.randint(-230, 230)
-        ts.goto(start_x, start_y) #먹이를 다른 곳으로 이동
-
-    if t.distance(te) >= 12: #주인공과 악당의 거리가 12이상이면
-        t.ontimer(play, 100) #0.1초 후 play 함수 실행(게임 계속)
-
+        point.goto(start_x, start_y)
+    
+    if playing: # 게임 플레이 중이면
+        t.ontimer(play, 100) # 0.1초 후 play 함수 실행
+    
 t.setup(500, 500)
 t.bgcolor("orange")
 
 t.shape("turtle")
 t.speed(0)
-t.up
-t.color("white")
+t.up()
+t.color("blue")
 t.onkeypress(turn_right, "Right")
 t.onkeypress(turn_up, "Up")
 t.onkeypress(turn_left, "Left")
 t.onkeypress(turn_down, "Down")
+t.onkeypress(start, 'space')
 
 t.listen()
-play()
+play() # play 함수를 호출해서 게임을 시작
 
 t.mainloop()
